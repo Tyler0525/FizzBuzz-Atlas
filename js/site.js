353f8df7547c9f299a7c9382dcf01011ac8708a5
Tyler0525 <96202897+Tyler0525@users.noreply.github.com>
2022-01-18 14:18:24 -0800

    FizzBuzz Challenge

--- js/site.js
+++ js/site.js
@@ -1,13 +1,65 @@
 // display amesage to the user
-function getMessage() {
-
-    let userMessage = document.getElementById("message").value;
-    
-Swal.fire({
-    icon: 'success',
-    title: 'THE APP SAYS',
-    userMessage,
-})
-
-    console.log("Hello Console Log");
+function getValue() {
+        alert("hello App");
 }
+//Program Entry Point
+
+
+function getValues2(){
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    //check to see if they are in fact an Integer //
+if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+    // generate a list of numbers 
+    let numbers = generateNumbers(startValue, endValue);
+    displayNumbers(numbers);
+} else {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Only Intergers are allowed for BuzzFizz'
+    })
+}
+}
+    
+function generateNumbers(start, end){
+    let numbers=[];
+    for (let i= start; i<= end; i++){
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+function displayNumbers(numbers){
+
+    let templateRows = "";
+    for (let index = 0; index < numbers.length; index ++){
+        let number = numbers[index];
+        let result = "";
+
+        if (number % 15 == 0){
+            result="fizzbuzz";
+        }
+        else if (number % 5 == 0){
+            result="buzz";
+        }
+        else if (number % 3 == 0){
+            result="fizz";
+        } else {
+            result = number;
+        }
+        let row = `<tr><td class="${result}">${result}</td></tr>`;
+        templateRows += row;
+    }
+
+    document.getElementById("result").innerHTML = templateRows;
+}
+    
+
+
+  
+
