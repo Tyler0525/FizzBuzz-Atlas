d03aeeb541ceb35c926169f2e5675bc90575b01c
Tyler0525 <96202897+Tyler0525@users.noreply.github.com>
2022-01-19 08:19:00 -0800

    Updated Functions

--- js/site.js
+++ js/site.js
@@ -1,65 +1,66 @@
 // display amesage to the user
 function getValue() {
-        alert("hello App");
+    alert("hello App");
 }
 //Program Entry Point
 
 
-function getValues2(){
-    let startValue = document.getElementById("startValue").value;
-    let endValue = document.getElementById("endValue").value;
+function getValues2() {
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
 
-    startValue = parseInt(startValue);
-    endValue = parseInt(endValue);
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
 
     //check to see if they are in fact an Integer //
-if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
-    // generate a list of numbers 
-    let numbers = generateNumbers(startValue, endValue);
-    displayNumbers(numbers);
-} else {
-    Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Only Intergers are allowed for BuzzFizz'
-    })
-}
-}
-    
-function generateNumbers(start, end){
-    let numbers=[];
-    for (let i= start; i<= end; i++){
-        numbers.push(i);
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        // generate a list of numbers 
+        let numbers = generateNumbers(fizzValue, buzzValue);
+        displayNumbers(fbData);
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Only Intergers are allowed for BuzzFizz'
+        })
     }
-    return numbers;
 }
 
-function displayNumbers(numbers){
+function generateNumbers(fizzValue, buzzValue) {
+    let nreturnArray = [];
+    let isFizz = false
+    let isBuzz = false
+    for (let i = 1; i <= 100; i++) {
 
-    let templateRows = "";
-    for (let index = 0; index < numbers.length; index ++){
-        let number = numbers[index];
-        let result = "";
 
-        if (number % 15 == 0){
-            result="fizzbuzz";
-        }
-        else if (number % 5 == 0){
-            result="buzz";
-        }
-        else if (number % 3 == 0){
-            result="fizz";
+        // fizzValue = 3 
+        isFizz = index % fizzValue == 0;
+        isBuzz = index % buzzValue == 0;
+        if (isFizz && isBuzz) {
+            returnNumbers.push('fizzBuzz');
+        } else if (isStart) {
+            returnNumbers.push('fizz');
+        } else if (isEnd) {
+            renturnNumbers.push('buzz')
         } else {
-            result = number;
+            returnNumbers.push(index);
         }
-        let row = `<tr><td class="${result}">${result}</td></tr>`;
-        templateRows += row;
+        return returnArray;
     }
 
-    document.getElementById("result").innerHTML = templateRows;
-}
-    
+    //Display the Values to the page 
+    function displayData(fbData) {
+        let contentDiv = document.getElementById("results");
+        let content = "";
 
+        // Clear previous Data 
+        contentDiv.innerHTML = "";
 
-  
+        for (let i = 0; i < fbData.length; i++) {
+            let dataValue = fbData[i];
+            let datatElement = `<div class="col ${dataValue}">${dataValue}</div>`
+            content += datatElement;
 
+        }
+            //Write the results tot the page
+            contentDiv.innerHTML = content;
